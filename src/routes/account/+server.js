fe1b2d13e18ab74e430f0def807ef7d4af146aa8
D4r3d3vil <mulhamreacts@gmail.com>
2023-11-23 16:00:39 +0300

    Move db to static

--- src/routes/account/+server.js
+++ src/routes/account/+server.js
@@ -4,6 +4,5 @@ const db = new SimpleJsonDb('./static/Data.json');
 export async function POST({ request }) {
     const { username } = await request.json()
     if(db.has(username)) return json({status:400, message:'username already taken'})
-    db.set(username, '')
     return json({status:200})
 }
