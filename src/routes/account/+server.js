ca3ebf076a1be5b10d5469357e636c1ab956715b
D4r3d3vil <mulhamreacts@gmail.com>
2023-11-23 14:42:18 +0300

    Move db

--- src/routes/account/+server.js
+++ src/routes/account/+server.js
@@ -1,6 +1,6 @@
 import { json } from '@sveltejs/kit';
 import SimpleJsonDb from 'simple-json-db'
-const db = new SimpleJsonDb('Data.json');
+const db = new SimpleJsonDb('./src/Data.json');
 export async function POST({ request }) {
     const { username } = await request.json()
     if(db.has(username)) return json({status:400, message:'username already taken'})
