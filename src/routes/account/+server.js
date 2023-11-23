4adffb6c647d7d79984fcdbbf221e3370cf957a4
D4r3d3vil <mulhamreacts@gmail.com>
2023-11-23 16:20:02 +0300

    Move db to static

--- src/routes/account/+server.js
+++ src/routes/account/+server.js
@@ -1,8 +1,13 @@
-import { json } from '@sveltejs/kit';
+import { error, json } from '@sveltejs/kit';
 import SimpleJsonDb from 'simple-json-db'
 const db = new SimpleJsonDb('./static/Data.json');
 export async function POST({ request }) {
     const { username } = await request.json()
     if(db.has(username)) return json({status:400, message:'username already taken'})
-    return json({db:db.set.toString(), status:200})
+    try {
+        db.set(username, '')
+    } catch (error) {
+        let err = error        
+    }
+    return json({error:err, status:200})
 }
